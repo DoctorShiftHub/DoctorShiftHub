2620b9bf0c17f079af268f1acf8d4e83c8c769d6
Leonardo Nunes <leonunesbs@gmail.com>
2023-04-08 17:28:28 -0300

    feat(UI): clerkloading skeleton

--- apps/nextjs/app/login/page.tsx
+++ apps/nextjs/app/login/page.tsx
@@ -4,25 +4,31 @@ import {
   SignIn,
 } from "@clerk/nextjs/app-beta/client";
 
-export default async function Page() {
+export default function Page() {
   return (
     <div>
-      <ClerkLoading>
-        <div>Loading...</div>
-      </ClerkLoading>
-      <ClerkLoaded>
-        <div className="flex min-h-screen flex-col-reverse items-center justify-center gap-4 lg:flex-row-reverse">
-          <SignIn></SignIn>
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="max-w-md py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+      <div className="flex min-h-screen flex-col-reverse items-center justify-center gap-4 lg:flex-row-reverse">
+        <ClerkLoaded>
+          <SignIn />
+        </ClerkLoaded>
+        <ClerkLoading>
+          <div
+            role="status"
+            className="flex h-[400px] w-full max-w-sm animate-pulse items-center justify-center rounded-lg bg-gray-300 dark:bg-gray-700"
+          >
+            <span className="invisible">Loading...</span>
+            <span className="sr-only">Loading...</span>
           </div>
+        </ClerkLoading>
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="max-w-md py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-      </ClerkLoaded>
+      </div>
     </div>
   );
 }
