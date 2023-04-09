a2130ec969dc039852c7f84c159f7e3b5757248f
Leonardo Nunes <leonunesbs@gmail.com>
2023-04-09 17:28:23 -0300

    feat(fix): teste

--- apps/nextjs/app/(home)/layout.tsx
+++ apps/nextjs/app/(home)/layout.tsx
@@ -6,8 +6,8 @@ export default function HomeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-base-200">
-      <header className="py-6 px-2 md:px-4">Header</header>
+    <div>
+      <header className="bg-base-200 py-6 px-2 md:px-4">Header</header>
       <main className="py-6 px-2 md:px-4">{children}</main>
       <footer className="py-6 px-2 md:px-4">Footer</footer>
     </div>
