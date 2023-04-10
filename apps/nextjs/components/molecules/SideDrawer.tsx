6043bfd05a7a4624eb82b255d603b912360a87f0
Leonardo Nunes <leonunesbs@gmail.com>
2023-04-10 19:15:21 -0300

    feat(add): imperative drawer control

--- apps/nextjs/components/molecules/SideDrawer.tsx
+++ apps/nextjs/components/molecules/SideDrawer.tsx
@@ -1,21 +1,33 @@
+"use client";
+
+import { ReactNode, useRef } from "react";
+
 import Link from "next/link";
-import { ReactNode } from "react";
 
 interface SideDrawerProps {
   children?: ReactNode;
 }
 
 export default function SideDrawer({}: SideDrawerProps) {
+  const drawerRef = useRef<HTMLLabelElement>(null);
   return (
     <div className="drawer-side">
-      <label htmlFor="sideDrawer" className="drawer-overlay"></label>
-      <ul className="menu menu-vertical bg-base-100 w-80 p-4">
+      <label
+        ref={drawerRef}
+        htmlFor="sideDrawer"
+        className="drawer-overlay"
+      ></label>
+      <ul className="menu menu-vertical bg-base-100 w-80 px-2 py-6 md:px-4">
         {/* <!-- Sidebar content here --> */}
         <li>
-          <Link href="/feed">Feed</Link>
+          <Link href="/feed" onClick={() => drawerRef.current?.click()}>
+            Feed
+          </Link>
         </li>
         <li>
-          <Link href="/agenda">Agenda</Link>
+          <Link href="/agenda" onClick={() => drawerRef.current?.click()}>
+            Agenda
+          </Link>
         </li>
       </ul>
     </div>
