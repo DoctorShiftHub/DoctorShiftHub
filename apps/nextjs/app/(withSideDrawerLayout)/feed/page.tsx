cd4a8f254d4da92a9e0b4ffcf0329b13103c33e3
Leonardo Nunes <leonunesbs@gmail.com>
2023-04-11 21:50:29 -0300

    feat(add): button scroll

--- apps/nextjs/app/(withSideDrawerLayout)/feed/page.tsx
+++ apps/nextjs/app/(withSideDrawerLayout)/feed/page.tsx
@@ -1,13 +1,37 @@
 "use client";
 
-import { trpc } from "@/nextjs/utils/trpc";
-
 export default function Page() {
-  const { data, status } = trpc.post.all.useQuery();
-  console.log(data);
   return (
-    <div>{`hello  ${
-      status === "success" ? data[0]?.title : "carregando"
-    } `}</div>
+    <div className="grid grid-cols-1 gap-2 px-2 sm:grid-cols-2 md:grid-cols-3 md:gap-4 md:px-4">
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div
+          key={i}
+          className="card card-compact md:card-normal dark:bg-neutral dark:text-neutral-content scroll-smooth shadow dark:shadow-none"
+          style={{
+            scrollSnapType: "x mandatory",
+          }}
+        >
+          <div
+            className="card-body"
+            style={{
+              scrollSnapAlign: "start",
+            }}
+          >
+            <h2 className="card-title">Médico Generalista</h2>
+            <ul>
+              <li>Hospital do Monte Castelo</li>
+              <li className="mb-2">Teresina - PI</li>
+              <li>
+                <span className="font-bold">Valor:</span> R$ 800,00
+              </li>
+            </ul>
+            <div className="btn-group justify-end">
+              <button className="btn btn-primary btn-outline">Ver mais</button>
+              <button className="btn btn-primary">Assumir</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
--- apps/nextjs/components/organisms/SubHeader.tsx
+++ apps/nextjs/components/organisms/SubHeader.tsx
@@ -1,25 +1,31 @@
 import { ReactNode } from "react";
-
+import ScrollSubHeaderButton from "../atoms/ScrollSubHeaderButton";
 interface SubHeaderProps {
   children?: ReactNode;
 }
 
 export default function SubHeader({}: SubHeaderProps) {
   return (
-    <div className="my-6 flex gap-2 overflow-x-auto px-2 md:px-4">
-      {Array.from({ length: 10 }).map((_, i) => (
-        <div
-          key={i}
-          className="card card-bordered card-compact md:card-normal bg-primary text-base-100"
-        >
-          <div className="card-body">
-            <div className="flex items-center gap-2">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
-              <button className="btn btn-secondary">Click Me</button>
+    <div className="relative">
+      <ScrollSubHeaderButton />
+      <div
+        id="promoted"
+        className="my-6 flex gap-2 overflow-x-auto px-2 md:px-4"
+      >
+        {Array.from({ length: 10 }).map((_, i) => (
+          <div
+            key={i}
+            className="card card-bordered card-compact md:card-normal bg-primary text-primary-content shadow dark:shadow-none"
+          >
+            <div className="card-body">
+              <div className="flex items-center gap-2">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing</p>
+                <button className="btn btn-secondary">Assumir</button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
