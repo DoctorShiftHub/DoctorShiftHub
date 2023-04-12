196858c283fb81eb27493d6f3c221b6d4f50e3dc
Leonardo Nunes <leonunesbs@gmail.com>
2023-04-11 22:15:06 -0300

    feat(UI): upgrade feed UI

--- apps/nextjs/app/(withSideDrawerLayout)/feed/page.tsx
+++ apps/nextjs/app/(withSideDrawerLayout)/feed/page.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  RxCalendar,
+  RxLightningBolt,
+  RxMoon,
+  RxSewingPin,
+  RxSun,
+} from "react-icons/rx";
+
 export default function Page() {
   return (
     <div className="grid grid-cols-1 gap-2 px-2 sm:grid-cols-2 md:grid-cols-3 md:gap-4 md:px-4">
@@ -17,17 +25,39 @@ export default function Page() {
               scrollSnapAlign: "start",
             }}
           >
+            <div className="text-primary flex justify-center gap-4 text-2xl">
+              <RxSun />
+              <RxMoon />
+            </div>
+            <div className="divider my-0 py-0"></div>
             <h2 className="card-title">Médico Generalista</h2>
-            <ul>
-              <li>Hospital do Monte Castelo</li>
-              <li className="mb-2">Teresina - PI</li>
+            <ul className="space-y-2">
               <li>
-                <span className="font-bold">Valor:</span> R$ 800,00
+                <div className="flex items-center gap-2">
+                  <RxSewingPin className="text-primary text-xl" />
+                  <span>Hospital do Monte Castelo, Teresina - PI</span>
+                </div>
               </li>
+              <li>
+                <div className="flex items-center gap-2">
+                  <RxCalendar className="text-primary text-xl" />
+                  <span>11/04/2023 - 19h~07h</span>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center gap-2">
+                  <RxLightningBolt className="text-primary text-xl" />
+                  <span>R$ 800,00</span>
+                </div>
+              </li>
+              <li></li>
             </ul>
             <div className="btn-group justify-end">
               <button className="btn btn-primary btn-outline">Ver mais</button>
               <button className="btn btn-primary">Assumir</button>
+            </div>
+            <div className="card-actions justify-end gap-2">
+              <div className="badge badge-outline">Até: 11/04/2023</div>
             </div>
           </div>
         </div>
